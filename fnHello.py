3dd0e9e26d09918bddfe0d27bc75a673bbd00cb7
hudhaifahz <hudhaifahz@gmail.com>
2018-04-14 16:50:03 -0700

    moved stuff around

--- fnHello.py
+++
@@ -1,7 +0,0 @@
-def print_hello(x):
-    print('hello'+x)
-    return 'hello'+x
-
-x = print_hello('rawr')
-print_hello(x)
-print_hello('kill')
--- newfriend.py
+++
@@ -1,4 +0,0 @@
-name = 'Emma'
-number = 3
-#print ('My new friend is ',name,' who speaks ',number,' languages.')
-print 'My new friend is',name,'who speaks',number,'languages.'
